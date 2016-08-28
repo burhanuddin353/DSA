04f6bac7fefa603b9a90b8247c5289bdacad7f19
Burhanuddin Sunelwala <burhanuddin353@gmail.com>
2016-08-28 10:12:50 +0400

    initial commit with two algorithms, fibonacci and gcd

---
+++ ADS/ADS/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ADS
+//
+//  Created by Burhanuddin Sunelwala on 8/28/16.
+//  Copyright © 2016 TFT. All rights reserved.
+//
+
+import Foundation
+
+print(Algorithm.gcd(3918848, 1653264))//print(Algorithm.gcd(numbers.cha[0].toUInt64, numbers[1])
+
